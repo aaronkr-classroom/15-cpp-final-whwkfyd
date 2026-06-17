cdafaa82fc410e99198d9fb169c3db81604cabe0
ththfql <145094239+ththfql@users.noreply.github.com>
2026-06-17 15:18:21 +0900

    end

---
+++ Chp 13/Intl.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+
+#include "Intl.h"
+
+istream& read_hw(istream& in, vector<double>& hw);
+
+istream& Intl::read(istream& in) {
+	Core::read_common(in);
+	in >> toeic;
+	read_hw(in, homework); // ??
+	return in;
+}
+
+double Intl::grade() const {
+	return min(Core::grade(), toeic);
+}
